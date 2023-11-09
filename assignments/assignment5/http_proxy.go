48b2f9f789d9bf80d3b6d77e26eb31ed168ce911
cpetrella-sketch <78937129+cpetrella-sketch@users.noreply.github.com>
2023-11-08 22:17:18 -0500

    Basic Handler
    Added basic handler code from one of the tutorials.

--- assignments/assignment5/http_proxy.go
+++ assignments/assignment5/http_proxy.go
@@ -13,6 +13,11 @@
 	"net/http"
  )
 
+ func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+ }
+
  func main() {
-	
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
  }
