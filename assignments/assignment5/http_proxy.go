e091dd02cfbfa26a10f14aa87a9d991cf568387d
cpetrella-sketch <78937129+cpetrella-sketch@users.noreply.github.com>
2023-11-10 19:41:11 -0500

    Maybe Basic working
    Still need to test it, but I think I have the very basics working now.

--- assignments/assignment5/http_proxy.go
+++ assignments/assignment5/http_proxy.go
@@ -10,16 +10,31 @@
  import (
 	"fmt"
 	"os"
+	"io"
 	"net/http"
  )
 
  func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Printf("got / request\n")
+	destination := r.URL.String()
+	response, err := http.Get(destination)
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+	defer response.Body.Close()
+	_, err = io.Copy(w, response.Body)
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
 	io.WriteString(w, "This is my website!\n")
  }
 
  func main() {
 	port := os.Args[1]
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(port, nil))//Note: change port number to be not hard coded.
+	err := (http.ListenAndServe(":"+port, nil))
+	if err != nil {
+		fmt.Println("Connection error")
+	}
  }
