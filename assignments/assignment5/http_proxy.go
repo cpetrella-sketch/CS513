5ef4d11e57b27bb591a0b91b5cc6394ff4cd60f9
cpetrella-sketch <78937129+cpetrella-sketch@users.noreply.github.com>
2023-11-16 20:26:37 -0500

    tests 1 and 2 passing

--- assignments/assignment5/http_proxy.go
+++ assignments/assignment5/http_proxy.go
@@ -1,41 +1,45 @@
 /*****************************************************************************
- * http_proxy.go                                                                 
- * Names: 
+ * http_proxy.go
+ * Names:
  * NetIds:
  *****************************************************************************/
 
- // TODO: implement an HTTP proxy
- package main
+// TODO: implement an HTTP proxy
+package main
 
- import (
+import (
 	"fmt"
-	"os"
 	"io"
 	"net/http"
- )
+	"os"
+)
 
- func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("There!")//debugger statement
+func handler(w http.ResponseWriter, r *http.Request) {
 	destination := r.URL.String()
+	fmt.Println(destination) //debugger statement
 	response, err := http.Get(destination)
+	fmt.Println((response.Status))
 	if err != nil {
 		io.WriteString(w, err.Error())
 		return
 	}
 	defer response.Body.Close()
-	_, err = io.Copy(w, response.Body)
-	if err != nil {
-		io.WriteString(w, err.Error())
-		return
+	w.WriteHeader(response.StatusCode)
+	if response.StatusCode == http.StatusOK {
+		_, err = io.Copy(w, response.Body)
+		if err != nil {
+			io.WriteString(w, err.Error())
+			return
+		}
 	}
- }
+}
 
- func main() {
+func main() {
 	port := os.Args[1]
 	http.HandleFunc("/", handler)
-	fmt.Println(port)//making sure port got passed in
+	fmt.Println(port) //making sure port got passed in
 	err := (http.ListenAndServe(":"+port, nil))
 	if err != nil {
 		fmt.Println("Connection error")
 	}
- }
+}
